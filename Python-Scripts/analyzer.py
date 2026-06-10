44d7e29d62bca03f89ff655db998a7dcecafa3be
Eman Fatima <jutteman80@gmail.com>
2026-06-10 22:35:33 +0500

    Improved README and project documentation

--- Python-Scripts/analyzer.py
+++ Python-Scripts/analyzer.py
@@ -1,1 +1,19 @@
-# Email Header Analyzer 
+# Simple Phishing Email Analyzer (SOC Level Beginner Tool)
+
+email = open("Samples/email1.txt", "r").read()
+
+print("=== EMAIL ANALYSIS REPORT ===\n")
+
+# Check sender
+if "paypal" in email or "security" in email:
+    print("[!] Suspicious sender detected")
+
+# Check urgency keywords
+if "urgent" in email.lower():
+    print("[!] Urgent language detected (possible phishing)")
+
+# Check fake links
+if "http://" in email:
+    print("[!] Insecure link found")
+
+print("\n=== ANALYSIS COMPLETE ===")# Email Header Analyzer 
